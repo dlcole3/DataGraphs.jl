012e223c9ee89eb3bdf12c46bb7193470c7f345d
dlcole3 <dlcole3@wisc.edu>
2022-10-13 12:09:40 -0500

    Added some basic interface functions and some docstrings

--- src/datagraphs/interface.jl
+++ src/datagraphs/interface.jl
@@ -1,12 +1,12 @@
-function get_node_data(dg::DataGraph, node::Any, weight::String=dg.node_data.attributes[1])
+function get_node_data(dg::DataGraphUnion, node::Any, attribute::String=dg.node_data.attributes[1])
     node_map  = dg.node_map
     node_data = dg.node_data
     attribute_map = node_data.attribute_map
 
-    return node_data.data[node_map[node], attribute_map[weight]]
+    return node_data.data[node_map[node], attribute_map[attribute]]
 end
 
-function get_edge_data(dg::DataGraph, node1, node2, weight::String=dg.edge_data.attributes[1])
+function get_edge_data(dg::DataGraph, node1, node2, attribute::String=dg.edge_data.attributes[1])
     edge_map  = dg.edge_map
     edge_data = dg.edge_data
     node_map  = dg.node_data
@@ -14,10 +14,10 @@ function get_edge_data(dg::DataGraph, node1, node2, weight::String=dg.edge_data.
 
     edge = _get_edge(node_map[node1], node_map[node2])
 
-    return edge_data.data[edge_map[edge], attribute_map[weight]]
+    return edge_data.data[edge_map[edge], attribute_map[attribute]]
 end
 
-function get_edge_data(dg::DataGraph, edge_tuple::Tuple{Any, Any}, weight::String=dg.edge_data.attributes[1])
+function get_edge_data(dg::DataGraph, edge_tuple::Tuple{Any, Any}, attribute::String=dg.edge_data.attributes[1])
     edge_map  = dg.edge_map
     edge_data = dg.edge_data
     node_map  = dg.node_data
@@ -25,13 +25,34 @@ function get_edge_data(dg::DataGraph, edge_tuple::Tuple{Any, Any}, weight::Strin
 
     edge = _get_edge(node_map[edge_tuple[1]], node_map[edge_tuple[2]])
 
-    return edge_data.data[edge_map[edge], attribute_map[weight]]
+    return edge_data.data[edge_map[edge], attribute_map[attribute]]
 end
 
-function get_node_data(dg::DataGraph)
+function get_node_data(dg::DataGraphUnion)
     return dg.node_data
 end
 
-function get_edge_data(dg::DataGraph)
+function get_edge_data(dg::DataGraphUnion)
     return dg.edge_data
 end
+
+function ne(dg::DataGraphUnion)
+    length(dg.edges)
+end
+
+function nn(dg::DataGraphUnion)
+    length(dg.nodes)
+end
+
+function nv(dg::DataGraphUnion)
+    nn(dg)
+end
+
+
+# Add set node_data
+# Add set edge_data
+# Add get_node positions
+# Add get_edge positions
+# Add number nodes
+# Add number edges
+# Consider changing interface functions; some functions apply to DataGraphUnions, and I should consider where to put these

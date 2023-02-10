d9ea507fa586d040be2f411f30024caa49517907
dlcole3 <dlcole3@wisc.edu>
2023-02-10 14:26:08 -0600

    Added path functions

--- src/functions.jl
+++ src/functions.jl
@@ -117,7 +117,177 @@ function Graphs.degree_centrality(dg::D) where {D <: DataGraphUnion}
     return degree_centrality(dg.g)
 end
 
+"""
+    average_degree(datagraph)
+
+Returns the average degree for `datagraph`
+"""
 function average_degree(dg::D) where {D <: DataGraphUnion}
     degrees = Graphs.degree(dg)
     return sum(degrees) / length(degrees)
+end
+
+"""
+    Graphs.has_path(datagraph, src_node, dst_node)
+
+Returns true if a path exists in the `datagraph` between `src_node` to `dst_node`. Else returns false
+"""
+function Graphs.has_path(dg::D, src_node, dst_node) where {D <: DataGraphUnion}
+    node_map = dg.node_map
+    nodes    = dg.nodes
+
+    if !(src_node in nodes && dst_node in nodes)
+        error("User has passed a node that does not exist in the DataGraph")
+    end
+
+    src_index = node_map[src_node]
+    dst_index = node_map[dst_node]
+
+    return Graphs.has_path(dg.g, src_index, dst_index)
+end
+
+"""
+    Graphs.has_path(datagraph, src_node, intermediate_node, dst_node)
+
+Returns true if a path exists in the `datagraph` between `src_node` and `dst_node` which
+passes through the `intermediate node`. Else returns false
+"""
+function Graphs.has_path(dg::D, src_node, intermediate_node, dst_node) where {D <: DataGraphUnion}
+    node_map = dg.node_map
+    nodes    = dg.nodes
+
+    if !(src_node in nodes && intermediate_node in nodes && dst_node in nodes)
+        error("User has passed a node that does not exist in the DataGraph")
+    end
+
+    src_index = node_map[src_node]
+    int_index = node_map[intermediate_node]
+    dst_index = node_map[dst_node]
+
+    if Graphs.has_path(dg.g., src_index, int_index) && Graphs.has_path(dg.g, int_index, dst_node)
+        return true
+    else
+        return false
+    end
+
+end
+
+"""
+    get_path(datagraph, src_node, dst_node; algorithm = "Dijkstra")
+
+Returns the shortest path in the `datagraph` between `src_node` and `dst_node`.
+Shortest path is computed by Dijkstra's algorithm
+
+`algorithm` is a string key word. Options are limited to "Dijkstra",
+"BellmanFord", "DEsopoPape", "FloydWarshall", and "Johnson"
+"""
+function get_path(dg::D, src_node, dst_node; algorithm = "Dijkstra") where {D <: DataGraphUnion}
+    node_map = dg.node_map
+    nodes    = dg.nodes
+
+    if !(src_node in nodes && dst_node in nodes)
+        error("User has passed a node that does not exist in the DataGraph")
+    end
+
+    src_index = node_map[src_node]
+    dst_index = node_map[dst_node]
+
+    if algorithm == "Dijkstra"
+        path_state = Graphs.dijkstra_shortest_paths(dg.g, [src_index])
+    elseif algorithm == "BellmanFord"
+        path_state = Graphs.bellman_ford_shortest_paths(dg.g, [src_index])
+    elseif algorithm == "DEsopoPape"
+        path_state = Graphs.desopo_pape_shortest_paths(dg.g, [src_index])
+    elseif algorithm == "FloydWarshall"
+        path_state = Graphs.floyd_warshall_shortest_paths(dg.g, [src_index])
+    elseif algorithm == "Johnson"
+        path_state = Graphs.johnson_shortest_paths(dg.g, [src_index])
+    else
+        error("$algorithm is not a supported algorithm option")
+    end
+
+    index_path = Graphs.enumerate_paths(path_state, dst_index)
+
+    if length(index_path) == 0
+        println("Path between nodes does not exist")
+        return []
+    end
+
+    path = Vector{Any}(undef, length(index_path))
+
+    for i in 1:length(index_path)
+        path[i] = nodes[index_path[i]]
+    end
+
+    return path
+end
+
+"""
+    get_path(datagraph, src_node, intermediate_node, dst_node; algorithm = "Dijkstra")
+
+Returns the shortest path in the `datagraph` between `src_node` and `dst_node`
+which passes through `intermediate node`.
+
+`algorithm` is a string key word. Options are limited to "Dijkstra",
+"BellmanFord", "DEsopoPape", "FloydWarshall", and "Johnson"
+"""
+function get_path_with_intemediate(dg::D, src_node, intermediate_node, dst_node; algorithm = "Dijkstra") where {D <: DataGraphUnion}
+    node_map = dg.node_map
+    nodes    = dg.nodes
+
+    if !(src_node in nodes && intermediate_node in nodes && dst_node in nodes)
+        error("User has passed a node that does not exist in the DataGraph")
+    end
+
+    src_index = node_map[src_node]
+    int_index = node_map[intermediate_node]
+    dst_index = node_map[src_node]
+
+    if algorithm == "Dijkstra"
+        path_state_src = Graphs.dijkstra_shortest_paths(dg.g, [src_index])
+        path_state_int = Graphs.dijkstra_shortest_paths(dg.g, [int_index])
+    elseif algorithm == "BellmanFord"
+        path_state_src = Graphs.bellman_ford_shortest_paths(dg.g, [src_index])
+        path_state_int = Graphs.bellman_ford_shortest_paths(dg.g, [int_index])
+    elseif algorithm == "DEsopoPape"
+        path_state_src = Graphs.desopo_pape_shortest_paths(dg.g, [src_index])
+        path_state_int = Graphs.desopo_pape_shortest_paths(dg.g, [int_index])
+    elseif algorithm == "FloydWarshall"
+        path_state_src = Graphs.floyd_warshall_shortest_paths(dg.g, [src_index])
+        path_state_int = Graphs.floyd_warshall_shortest_paths(dg.g, [int_index])
+    elseif algorithm == "Johnson"
+        path_state_src = Graphs.johnson_shortest_paths(dg.g, [src_index])
+        path_state_int = Graphs.johnson_shortest_paths(dg.g, [int_index])
+    else
+        error("$algorithm is not a supported algorithm option")
+    end
+
+    path_state_src = Graphs.dijkstra_shortest_paths(dg.g, [src_index])
+
+    index_path_to_int = Graphs.enumerate_paths(path_state_src, int_index)
+
+    path_state_int = Graphs.sijkstra_shortest_paths(dg.g, [int_index])
+
+    index_path_to_dst = Graphs.enumerate_paths(path_state_int, dst_index)
+
+    if length(index_path_to_int) == 0 || length(index_path_to_dst) == 0
+        println("Path through intermediate node does not exist")
+        return []
+    end
+
+    path = Vector{Any}(undef, (length(index_path_to_int) + length(index_path_to_dst) - 1))
+    nodes = dg.nodes
+
+    path_to_int_len = length(index_path_to_int)
+    path_to_dst_len = length(index_path_to_dst)
+
+    for i in 1:path_to_int_len
+        path[i] = nodes[index_path[i]]
+    end
+
+    for i in 2:path_to_int_dst
+        path[i + path_to_int_len - 1] = nodes[index_path_to_dst[i]]
+    end
+
+    return path
 end

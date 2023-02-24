0d8732f47ad0c99ec67b35813b5b27e88e2cb877
dlcole3 <dlcole3@wisc.edu>
2023-02-23 19:46:55 -0600

    edited docstring

--- src/functions.jl
+++ src/functions.jl
@@ -474,7 +474,7 @@ end
 """
     order_edges!(dg) where {D <: DataGraphUnion}
 
-Arranges the edges of `dg` so that they follow the order of `dg.nodes`. For `DataGraph`s, this
+Arranges in place the edges of `dg` so that they follow the order of `dg.nodes`. For `DataGraph`s, this
 means all edges connected to `dg.nodes[1]` are ordered first, and so on. For `DataDiGraph`s, this
 means that all edges originating at `dg.nodes[1]` are ordered first, and so on for `length(dg.nodes)`
 """
